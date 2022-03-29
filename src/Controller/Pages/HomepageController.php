ac81fb275b043cf8600d915c439af57b600f01f2
Szymon99d <sheep01999@gmail.com>
2022-03-29 17:19:17 +0000

    Render user registration form

--- src/Controller/Pages/HomepageController.php
+++ src/Controller/Pages/HomepageController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller\Pages;
 
+use App\Entity\User;
+use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -10,10 +13,21 @@ use Symfony\Component\HttpFoundation\Response;
 class HomepageController extends AbstractController
 {
     #[Route('/',name:'app_homepage')]
-    public function homepage(): Response
+    public function homepage(Request $request): Response
     {
-        return $this->render('pages/homepage.html.twig');
+        $user = new User;
+        $userForm = $this->createForm(UserType::class,$user);
+        $userForm->handleRequest($request);
+        if($userForm->isSubmitted() && $userForm->isValid())
+        {
+            $user = $userForm->getData();
+            $this->redirectToRoute('app_homepage');
+        }
+        return $this->renderForm('pages/homepage.html.twig',[
+            'form'=>$userForm,
+        ]);
     }
+
 }
 
 
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,11 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +17,24 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('password')
-            ->add('username')
+            ->add('email', EmailType::class,[
+                'label'=>'Email address',
+                'attr'=>['class'=>'form-control mb-3','placeholder'=>'youremailaddress@gmail.com']
+            ])
+            ->add('password',RepeatedType::class,[
+                'type'=>PasswordType::class,
+                'first_name'=>'Password',
+                'second_name'=>'RepeatPassword',
+                'first_options'=>['label'=>'Password','attr'=>['placeholder'=>'Password','class'=>'form-control mb-3']],
+                'second_options'=>['label'=>'Repeat password','attr'=>['placeholder'=>'Repeat password','class'=>'form-control mb-3']]
+            ])
+            ->add('username',TextType::class,[
+                'label'=>'Username',
+                'attr'=>['class'=>'form-control mb-3','placeholder'=>'John67']
+            ])
+            ->add('submit',SubmitType::class,[
+                'attr'=>['class'=>'btn btn-success w-100','value'=>'Submit']
+            ])
         ;
     }
 
