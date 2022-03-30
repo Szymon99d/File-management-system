cf72329be115b52ad21da23c0fbf33ce1eadf369
Szymon99d <sheep01999@gmail.com>
2022-03-30 18:02:43 +0000

    Add register user service

--- src/Controller/Pages/HomepageController.php
+++ src/Controller/Pages/HomepageController.php
@@ -4,28 +4,27 @@ namespace App\Controller\Pages;
 
 use App\Entity\User;
 use App\Form\UserType;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Services\RegisterUserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
-
 class HomepageController extends AbstractController
 {
     #[Route('/',name:'app_homepage')]
-    public function homepage(Request $request, EntityManagerInterface $em): Response
+    public function homepage(Request $request, RegisterUserService $registerUserService): Response
     {
         $user = new User;
         $userForm = $this->createForm(UserType::class,$user);
         $userForm->handleRequest($request);
         if($userForm->isSubmitted() && $userForm->isValid())
         {
-            $user = $userForm->getData();
-            $em->persist($user);
-            $em->flush();
+            $email = $userForm->get('email')->getData();
+            $username = $userForm->get('username')->getData();
+            $plainpassword = $userForm->get('password')->getData();
+            $registerUserService->registerUser($user,$email,$username,$plainpassword);
             $this->redirectToRoute('app_homepage');
-            //Redirect to user panel TODO later
         }
         return $this->renderForm('pages/homepage.html.twig',[
             'form'=>$userForm,
---
+++ src/Services/RegisterUserService.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Services;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Doctrine\ORM\Exception\ORMException;
+
+class RegisterUserService
+{
+    private $entityManager;
+    private $passwordHasher;
+    public function __construct(EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->entityManager = $entityManager;
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function hashPassword(User $user, string $plainpassword): string
+    {
+        return $this->passwordHasher->hashPassword($user, $plainpassword);
+    }
+
+    public function registerUser(User $user, string $email, string $username, string $password): void
+    {
+        try{
+            $user->setEmail($email);
+            $user->setUsername($username);
+            $user->setPassword($this->hashPassword($user,$password));
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+        }
+        catch(ORMException $e){
+            throw $e;
+        }
+        
+    }
+
+}
+
+
+
+?>
