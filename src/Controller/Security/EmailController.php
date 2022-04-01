cf1879f92562e7f827ddabd11f63720839c977e2
Szymon99d <sheep01999@gmail.com>
2022-04-01 19:22:08 +0000

    Validate email confirmation

---
+++ src/Controller/Security/EmailController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller\Security;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
+use SymfonyCasts\Bundle\VerifyEmail\VerifyEmailHelperInterface;
+
+class EmailController extends AbstractController
+{
+    #[Route('/verify-email',name: 'app_verify_email')]
+    public function verifyEmail(Request $request, VerifyEmailHelperInterface $verifyEmail, EntityManagerInterface $em): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $user = $this->getUser();
+        try {
+            $verifyEmail->validateEmailConfirmation($request->getUri(), $user->getId(), $user->getEmail());
+        } catch (VerifyEmailExceptionInterface $e) {
+            $this->addFlash('verify_email_error', $e->getReason());
+
+            return $this->redirectToRoute('app_register');
+        }
+        $user->setIsVerified(true);
+        $em->persist($user);
+        $em->flush();
+
+        $this->addFlash('success', 'Your e-mail address has been verified.');
+
+        return $this->redirectToRoute('app_homepage');
+    }
+}
+
+
+?>
