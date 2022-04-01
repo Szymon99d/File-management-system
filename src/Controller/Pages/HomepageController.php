89eed5b0be7c85f68bc869261bcd9ba01a762f3c
Szymon99d <sheep01999@gmail.com>
2022-04-01 19:26:18 +0000

    Add user authentication after registration

--- src/Controller/Pages/HomepageController.php
+++ src/Controller/Pages/HomepageController.php
@@ -4,17 +4,23 @@ namespace App\Controller\Pages;
 
 use App\Entity\User;
 use App\Form\UserType;
+use App\Security\UserAuthenticator;
 use App\Services\ConfirmEmailService;
 use App\Services\RegisterUserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
 class HomepageController extends AbstractController
 {
     #[Route('/',name:'app_homepage')]
-    public function homepage(Request $request, RegisterUserService $registerUserService,ConfirmEmailService $ces): Response
+    public function homepage(Request $request, 
+    RegisterUserService $registerUserService,
+    ConfirmEmailService $ces,
+    UserAuthenticator $userAuthenticator,
+    UserAuthenticatorInterface $userAuthenticatorInterface): Response
     {
         $user = new User;
         $userForm = $this->createForm(UserType::class,$user);
@@ -24,8 +30,9 @@ class HomepageController extends AbstractController
             $email = $userForm->get('email')->getData();
             $username = $userForm->get('username')->getData();
             $plainpassword = $userForm->get('password')->getData();
-            $ces->confirmEmail($email);
             $registerUserService->registerUser($user,$email,$username,$plainpassword);
+            $ces->confirmEmail($user);
+            $userAuthenticatorInterface->authenticateUser($user,$userAuthenticator,$request);
             $this->redirectToRoute('app_homepage');
         }
         return $this->renderForm('pages/homepage.html.twig',[
