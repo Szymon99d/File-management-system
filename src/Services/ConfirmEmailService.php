f6b1ee115c9b5410c3f9219961a4b7f5c378f96e
Szymon99d <sheep01999@gmail.com>
2022-04-01 19:21:00 +0000

    Add confirmation link in email

--- src/Services/ConfirmEmailService.php
+++ src/Services/ConfirmEmailService.php
@@ -2,26 +2,33 @@
 
 namespace App\Services;
 
+use App\Entity\User;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
+use SymfonyCasts\Bundle\VerifyEmail\VerifyEmailHelperInterface;
 
 class ConfirmEmailService
 {
     private $mailer;
-    public function __construct(MailerInterface $mailer)
+    private $verifyEmail;
+    public function __construct(MailerInterface $mailer, VerifyEmailHelperInterface $verifyEmail)
     {
         $this->mailer = $mailer;
+        $this->verifyEmail = $verifyEmail;
     }
-    public function confirmEmail(string $email)
+    public function confirmEmail(User $user)
     {
-        //Todo
+        $signatureComponents = $this->verifyEmail->generateSignature(
+            'app_verify_email',
+            $user->getId(),
+            $user->getEmail()
+        );
         $confirmationEmail = (new Email())
             ->from("admin@fms.local")
-            ->to($email)
+            ->to($user->getEmail())
             ->subject("Confirm your email!")
-            ->text("To confirm your email address click this link:");
-        
+            ->text("To confirm your email address click this link:".$signatureComponents->getSignedUrl()."");
         try {
             $this->mailer->send($confirmationEmail);
         } catch (TransportExceptionInterface $e) {
