ae3ff26640aa8aecce0014870b2d88e985f68370
Szymon99d <sheep01999@gmail.com>
2022-04-03 16:45:16 +0000

    Add route to panel

--- src/Controller/Pages/UserPanelController.php
+++ src/Controller/Pages/UserPanelController.php
@@ -3,12 +3,20 @@
 namespace App\Controller\Pages;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class UserPanelController extends AbstractController
 {
+    #[Route('/user-panel',name:'app_user_panel')]
+    public function userPanel(): Response
+    {
+        return $this->render('pages/user_panel.html.twig',[
+
+        ]);
+    }
+
 
 }
-
 
 ?>
