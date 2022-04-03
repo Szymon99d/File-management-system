cde069ab60b53e1c474d562e66defa7b8c1cde84
Szymon99d <sheep01999@gmail.com>
2022-04-03 17:29:13 +0000

    Check if user is authenticated and verifed

--- src/Controller/Pages/UserPanelController.php
+++ src/Controller/Pages/UserPanelController.php
@@ -11,6 +11,16 @@ class UserPanelController extends AbstractController
     #[Route('/user-panel',name:'app_user_panel')]
     public function userPanel(): Response
     {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $user = $this->getUser();
+        if(!$user->getIsVerified())
+        {
+            $this->addFlash('notice','Before you access your file panel, please confirm your email address.');
+            return $this->redirectToRoute('app_homepage');
+        }
+            
+
+        
         return $this->render('pages/user_panel.html.twig',[
 
         ]);
