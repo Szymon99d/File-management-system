18acdb63bacd8afc90a2405a382731ff4fe28156
Szymon99d <sheep01999@gmail.com>
2022-04-03 16:43:03 +0000

    Add user panel controller

---
+++ src/Controller/Pages/UserPanelController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller\Pages;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class UserPanelController extends AbstractController
+{
+
+}
+
+
+?>
